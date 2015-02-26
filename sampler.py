81de631e1f69207bce725a0f302c8e070175660d
David McGaughey <mcgaugheyd@mail.nih.gov>
2015-02-26 12:03:38 -0500

    Not functional yet. Added some more input option commands and the algorithm R function

--- sampler.py
+++ sampler.py
@@ -19,37 +19,40 @@ N% of any given line
 
 import argparse, random, itertools
 
-parser = argparse.ArgumentParser(description="Sample N percent of lines or groups \
+parser = argparse.ArgumentParser(description="Sample N lines or groups \
 								in a file")
-parser.add_argument("N",help="N percent lines to sample in the file", 
-					type=int)
+parser.add_argument("-p",,"--percent", help="P percent lines to sample in the file, \
+					may not be used with -n", type=int, default = 1)
+parser.add_argument("-n","--number", help="N number of lines to sample in the file, \
+					may not be used with -p", type=int)
 parser.add_argument("-head","--header", help="How many header lines to return",
 					type=int)
 parser.add_argument("-g", "--group", help="Return random lines in groups. Useful \
-					for when sets of lines are important, e.g. fastq files",
+					for when lines are grouped sequentially, e.g. fastq files",
 					type=int)
 parser.add_argument("--input", type=argparse.FileType("r"), default = "-")
 								
 args = parser.parse_args()
 
-N=args.N
+P=args.percent
 file = args.input
 header_lines = args.header
 group_size = args.group
 
 # function to print or not print a line randomly
-def returnN(line, N):
-	if random.randrange(0,100) < N:
+def returnN(line, P):
+	if random.randrange(0,100) < P:
 		print line[:-1]
 		return
 	else:
 		return
 
 # function to batch lines together in n groups
-def grouper(n, iterable, fillvalue=None):
-	args = [iter(iterable)] * n
+def grouper(g, iterable, fillvalue=None):
+	args = [iter(iterable)] * g
 	return itertools.izip_longest(fillvalue=fillvalue, *args)
 
+def reservoirSampling(line, N)
 
 # if there is a header
 if args.header >= 1:
@@ -65,7 +68,7 @@ if args.header >= 1:
 			# selected groups
 			else:
 				for line in grouper(group_size, file):
-					if random.randrange(0,100) < N:
+					if random.randrange(0,100) < P:
 						print ''.join(line)[:-1]
 					else:
 						continue		
@@ -79,19 +82,42 @@ if args.header >= 1:
 				line_count += 1
 			# after header, print random lines
 			else:
-				returnN(line, N)
+				returnN(line, P)
 
 # no header present
 else:
 	if group_size:						#if grouping is specified
 		for line in grouper(group_size, file):
-			if random.randrange(0,100) < N:
+			if random.randrange(0,100) < P:
 				print ''.join(line)[:-1]
 			else:
 				continue
 	else:							#grouping is not specified
 		for line in file:
-			returnN(line, N)
+			returnN(line, P)
 
 
-	
+
+
+
+
+
+
+
+line_storage = list()
+line_num = 0 
+for line in file:
+	if line_num < N - 1:
+		line_storage.append(line)
+	else:
+		j = random.randint(0,line_num)
+			if j < n:
+				line_storage[j] = line
+			else:
+				continue
+	line_num += 1
+
+for line in line_storage:
+	print line
+
+
