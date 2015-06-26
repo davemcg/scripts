904b4670675ff241c31f145b8f5bc869970c980d
David McGaughey <mcgaughey@gmail.com>
2015-06-26 17:24:03 -0400

    non-working

---
+++ cut_lines.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python2.7
+
+"""
+Takes in a file and outputs user-specified lines
+"""
+
+import argparse
+
+parser = argparse.ArgumentParser(description="Output user-specified lines from a file.")
+
+parser.add_argument("--input", type=argparse.FileType("r"), default = "-", help = \
+					"Input file. Can also take input from pipe.")
+parser.add_argument("-l","--list", type=argparse.FileType("r"), help = \
+					"File with lines (\n separated) to be returned.")
+
+args = parser.parse_args()
+
+lines_to_return = []
+for line in args.list:
+	lines_to_return.append(line[:-1])
+
+current_line = 1
+
+current_line in lines_to_return
+for line in args.input:
+	if current_line in lines_to_return:
+		print line[:-1]
+	else:
+		continue
+	current_line += 1 
