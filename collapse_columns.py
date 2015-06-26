2b48e3126f8767daef5473b4c43d1e25d40864c6
David McGaughey <mcgaughey@gmail.com>
2015-06-26 16:02:12 -0400

    Added options with argparse

--- collapse_columns.py
+++ collapse_columns.py
@@ -5,13 +5,24 @@ Merges adjacent columns in a pairwise manner:
 e.g. T T C T A G C G becomes TT CT AG CG
 """
 
-import fileinput
+import argparse 
 
-for line in fileinput.input():
+parser = argparse.ArgumentParser(description="Merge together adjacent columns in a pairwise manner. For example T T C T A G C G becomes TT CT AG CG. \
+								User can specify which column to begin on with -c.") 
+
+parser.add_argument("--input", type=argparse.FileType("r"), default = "-", help = \
+					"Input file. Can also take input from pipe.")
+parser.add_argument("-c","--column", type=int, default=1, help = "Which column to \
+					begin on.")
+
+args = parser.parse_args()
+
+
+for line in args.input:
 	# split on space
 	line = line.split()
 	length = len(line)
-	col_to_start = 5 
+	col_to_start = args.column 
 	output = []
 	# add non-merge beginning columns to list
 	for i in range(0,col_to_start-1,1):
