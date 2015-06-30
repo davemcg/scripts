72a5290d1bc79f7f6d8a24327ccb79fea4597739
David McGaughey <mcgaughey@gmail.com>
2015-06-30 09:25:26 -0400

    Renamed cut_lines

--- cut_rows.py
+++ cut_rows.py
@@ -1,22 +1,29 @@
 #!/usr/bin/env python2.7
 
 """
-Because unix cut won't process huge numbers of columns
-I have to write my own version
+Takes in a file and outputs user-specified columns 
 """
 
 import argparse
 
-parser = argparse.ArgumentParser(description="Output user-specified columns from a file.")
+parser = argparse.ArgumentParser(description="Output user-specified lines from a file.")
 
-parser.add_argument("--input", type=argparse.FileType("r"), default = "-", help = \ 
-                    "Input file. Can also take input from pipe.")
-parser.add_argument("-l","--list", type=argparse.FileType("r"), help = \ 
-                    "File with columns (\n separated) to be returned.")
-parser.add_argument("-d","--delim", type = str, default = "\t", help = "Delimiter. \
-					Default is \t")
+parser.add_argument("--input", type=argparse.FileType("r"), default = "-", help = \
+					"Input file. Can also take input from pipe.")
+parser.add_argument("-l","--list", type=argparse.FileType("r"), help = \
+					"File with lines (\n separated) to be returned.")
 
 args = parser.parse_args()
 
+lines_to_return = []
+for line in args.list:
+	lines_to_return.append(line[:-1])
 
-
+current_line = 1
+for line in args.input:
+	if str(current_line) in lines_to_return:
+		print line[:-1]
+		current_line += 1
+	else:
+		current_line += 1
+		continue
