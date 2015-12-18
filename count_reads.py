ca81cc13bf6ed5f5a186efd23ea9134e4b6c6d7a
David McGaughey <mcgaughey@gmail.com>
2015-12-18 17:15:01 -0500

    Runs samtools inside script now. I think

--- count_reads.py
+++ count_reads.py
@@ -2,9 +2,23 @@
 
 
 import sys
+import subprocess
 
+def run_samtools(bamfile, region):
+	samtools_input = 'samtools mpileup -r' + region + 
+		"-f /fdb/GATK_resource_bundle/hg19-2.8/ucsc.hg19.fasta" + 
+		bamfile 
+
+	samtools_view = (subprocess.check_output(samtools_input, shell=True)).decode('utf-8')
+	return(samtools_view)
 
 def process(samtools_input):
+	"""
+	Takes input from samtools mpileup (requires reference) and counts
+	numbers of A, C, G, T in pileup. 
+	
+	Currently only tested when fed a single line 
+	"""
 	line = samtools_input
 	reads = line[4]
 	reads = reads.lower()
@@ -38,7 +52,8 @@ def process(samtools_input):
 def main():
 	for line in sys.stdin:
 		line = line.split()
-		print("chr","position","Reference","ReadCoverage","A","C","G","T")
-		print(line[0],line[1],line[2],line[3],process(line))
+	#	print("chr","position","Reference","ReadCoverage","A","C","G","T")
+		samtools_return = run_samtools(sys.argv[1],sys.argv[2])
+		print(line[0],line[1],line[2],line[3],process(samtools_return))
 
 main()
