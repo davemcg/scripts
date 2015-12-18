05a9895485ef6299589f14240e65f2d17c617576
David McGaughey <mcgaughey@gmail.com>
2015-12-18 15:57:10 -0500

    Should identify reference in count

--- count_reads.py
+++ count_reads.py
@@ -8,15 +8,31 @@ def process(samtools_input):
 	line = samtools_input
 	reads = line[4]
 	reads = reads.lower()
-	a = str(reads.count('a'))
-	c = str(reads.count('c'))
-	g = str(reads.count('g'))
-	t = str(reads.count('t'))
-	ref = str(reads.count('.') + reads.count(','))
-	counts = '\t'.join([a,c,g,t,ref])
+	ref = line[2]
+
+	if reference == 'a':
+		a = str(reads.count('.') + reads.count(','))
+	else:
+		a = str(reads.count('a'))
+	if reference == 'c':
+		c = str(reads.count('.') + reads.count(','))
+	else:
+		c = str(reads.count('c'))
+	if reference == 'g':
+		g = str(reads.count('.') + reads.count(','))
+	else:
+		g = str(reads.count('g'))
+	if reference == 't':
+		t = str(reads.count('.') + reads.count(','))
+	else:
+		t = str(reads.count('t'))
+	counts = '\t'.join([a,c,g,t])
 	return(counts)
 
-for line in sys.stdin:
-	line = line.split()
-	print("chr","position","Reference","ReadCoverage","A","C","G","T","REF")
-	print(line[0],line[1],line[2],line[3],process(line))
+def main():
+	for line in sys.stdin:
+		line = line.split()
+		print("chr","position","Reference","ReadCoverage","A","C","G","T")
+		print(line[0],line[1],line[2],line[3],process(line))
+
+main()
