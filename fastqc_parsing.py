56fdd557995a914f45ce84d009cdca0f1e7472cb
davemcg <mcgaughey@gmail.com>
2016-04-11 14:41:59 -0400

    tons of little tweaks results in large overwrite. functions now. outputs a file for each category, with all different fastqc runs together

--- fastqc_parsing.py
+++ fastqc_parsing.py
@@ -1,40 +1,58 @@
 #!/usr/local/bin/python3
 
+import re
 import glob
 import subprocess
 import argparse
+import os
 
 parser = argparse.ArgumentParser(description=\
-        """ \
-        Scrapes folders of fastqc runs and extracts \
-        important info for large analyses. \
-        \
-        Just run in directory holding the fastqc folders. \
-        Will use the folder names to name the files. """)
+	""" \
+	Scrapes folders of fastqc runs and extracts \
+	important info for large analyses. \
+	\
+	Just run in directory holding the fastqc folders. \
+	Will use the folder names to name the files. """)
+parser.add_argument('-d','--directory', help='Give a \
+		directory to crawl that has the fastqc zip files')
 
+args = parser.parse_args()
+dir = args.directory
 
-zips = glob.glob('*.zip')
+if dir:
+	zips = glob.glob(dir + '/*zip')
+else:
+	zips = glob.glob('*.zip')
 
 #unzips the fastqc data
-[subprocess.call("unzip " + i, shell=True) for i in zips]
-
+#[subprocess.call("unzip " + i, shell=True) for i in zips]
 
 # roll through dirs and parse data 
-for i in zips[:=4]:
-    file = i + '/fastqc_data.txt'
-    data = file.readlines()
-    for i in data:
-        if i != '>>END_MODULE'
-            continue
-        if i[0:2] == '>>':
-            file_name = '_'.join(i.split()[0])
-            output = open(file_name,'w')
-        if re.match(i,'^#[A-Z]'):
-            i = i.strip() + '\tFilename\n'
-        if re.match(i,'^Filename'):
-            filename = i.split()[1]
-            output.write(i)
-        else:
-            i = i.strip() + '\t' + filename + '\n'
-            output.write(i)
+for i in zips:
+	i = i[:-4]
+	file = open(i + '/fastqc_data.txt','r')
+	data = file.readlines()
+	for line in data:
+		count = 0
+		if re.match('^>>END_MODULE',line):
+			continue
+		elif re.match('^##', line):
+			continue
+		elif re.match('^>>', line):
+			file_name = line.strip().split()
+			# grab fastqc pass, warn, fail tag
+			fastqc_filter = file_name[-1]
+			# get fastqc section name and use as filename
+			file_name[0] = file_name[0].split('>>')[1]
+			file_name = '_'.join(file_name[0:-1])
+			output = open(file_name,'a')
+		elif re.match('^Filename',line): #grab input file for FastQC to write to column
+			filename = line.split()[1]
+		elif re.match('^#[A-Z]',line): # IDs a new section and writes the header just once
+			line = line.strip() + '\tFilename' + '\tFastQC_Filter\n'
+			if os.stat(file_name).st_size == 0:
+				output.write(line[1:])
+		else:
+			line = line.strip() + '\t' + filename + '\t' + fastqc_filter + '\n'
+			output.write(line)
 
