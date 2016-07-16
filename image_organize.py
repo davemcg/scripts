3de7fe0397562a5fdb22497b1760541727753e87
David McGaughey <mcgaughey@gmail.com>
2016-07-16 17:19:13 -0400

    reworked to do a re find replace on filename

--- image_organize.py
+++ image_organize.py
@@ -4,6 +4,7 @@
 import argparse
 import subprocess
 import glob
+import re
 
 parser = argparse.ArgumentParser(description =\
 	"Moves image files into folders by date (Year-Month-Day)\
@@ -12,26 +13,27 @@ parser.add_argument('--directory', help =\
 	"Give the directory path to process files \
 	Example: \
    		~/git/./Sony_ARW_Rename_and_Organize.py ~/Pictures/2016/")
-parser.add_argument('--pattern', nargs='?', default="D*.ARW", help =\
-	"Give pattern to identify files to move. Default is \"D.*ARW\"")
-parser.add_argument('--prepend', nargs='?', default="", help =\
-	"Prepend to each file. Default is nothing.")
+parser.add_argument('--substitute', nargs='?', default="^", help =\
+	"Give pattern to substitute. Default is \"^\"")
+parser.add_argument('--replace', nargs='?', default='RX1RII_', help =\
+	"Replacement text to replace. Default is \"RX1RII_\"")
 parser.add_argument('--filetype', nargs='?', default="ARW", help =\
 	"Give file ending for exiftool to process. Default is ARW.")
 args = parser.parse_args()
 
 directory_path = args.directory
-pattern = args.pattern
-prepend = args.prepend
+substitute = args.substitute
+replace = args.replace
 filetype = args.filetype
 
-path_file = directory_path + '/' + pattern
+path_file = directory_path + '/' + '*' + filetype
 file_matches = glob.glob(path_file)
 
 # rename
 for image_file in file_matches:
 	file_name = image_file.split('/')[-1]
-	new_file = directory_path  + prepend + file_name
+	new_file_name = '/' + re.sub(substitute, replace, file_name)
+	new_file = directory_path  + new_file_name
 	# bash rename command
 	mv_command = 'mv ' + image_file + ' ' + new_file
 	subprocess.call(mv_command, shell='True')
