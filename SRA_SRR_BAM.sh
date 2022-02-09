59fc3ed41eb86cb35ad5425599ad6c67ef018c3c
David McGaughey <mcgaughey@gmail.com>
2022-02-09 18:14:04 -0500

    update

--- SRA_SRR_BAM.sh
+++ SRA_SRR_BAM.sh
@@ -2,6 +2,6 @@
 
 SRR=$1
 
-wget `wget -qO- https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=$SRR | grep aws | grep bam | sed 's/<[^>]*>/\n/g'`
-bam=$(basename `wget -qO- https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=$SRR | grep aws | grep bam | sed 's/<[^>]*>/\n/g'`)
+wget `wget -qO- https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=$SRR | grep href | grep bam | sed 's/<[^>]*>/\n/g'`
+bam=$(basename `wget -qO- https://trace.ncbi.nlm.nih.gov/Traces/sra/?run=$SRR | grep href | grep bam | sed 's/<[^>]*>/\n/g'`)
 mv $bam $SRR.bam
